d087d16e22ac6896d4934c543fb87db8d9f6458d
marwa <marwajawad19@gmail.com>
2021-08-29 08:54:40 -0400

    setup the redux stor

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import {createStore,applyMiddleware} from "redux"
+import {Provider} from "react-redux"
+import catReducer from "./features/cats/catsSlice"
+import thunkMiddleware from "redux-thunk" 
+import {composeWithDevTools} from "redux-devtools-extension"
+const composedEnhancer =composeWithDevTools(applyMiddleware(thunkMiddleware)) 
+const store =createStore(catReducer,composedEnhancer)
+ReactDOM.render(
+<Provider store={store}>
+<App /></Provider>, document.getElementById("root"));
