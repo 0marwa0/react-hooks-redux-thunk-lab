a209ec13486a08d26974dbaa941b79e3e2ecc38d
marwa <marwajawad19@gmail.com>
2021-08-29 10:38:25 -0400

    adding catlist component and passing feched cats into it

--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,10 +1,19 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import CatList from "./CatList"
+import {useSelector,useDispatch}from "react-redux"
+import { fetchCats } from "./catsSlice";
 function Cats() {
+  const dispatch =useDispatch()
+  const catPics=useSelector(state=>state.entities)
+   useEffect(()=>{
+    dispatch(fetchCats())
+    console.log("in app`");
+      },[dispatch])
+console.log(catPics,"here we go");
   return (
     <div>
       <h1>CatBook</h1>
-      {/* add CatList component here */}
+      <CatList catPics={catPics} />
     </div>
   );
 }
